02f2d8251c5814c6f357cb71849e6b43280c9288
Shreyasingh2110 <shreyasinghrajput2110@gmail.com>
2026-05-26 02:27:19 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-inventory-tracker-bylr.onrender.com/api",
 });
 
 // Automatically inject JWT Bearer token into headers
